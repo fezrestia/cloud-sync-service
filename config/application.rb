0b23d50d54c2c0469b3d8fd0846d83f0e3608019
Aki.SHIMO <fezrestia@gmail.com>
2018-02-28 13:09:04 +0900

    ADD: Support rails 5.x

--- config/application.rb
+++ config/application.rb
@@ -1,4 +1,4 @@
-require File.expand_path('../boot', __FILE__)
+require_relative 'boot'
 
 require 'rails/all'
 
@@ -14,21 +14,5 @@ module CloudSyncService
     # Settings in config/environments/* take precedence over those specified here.
     # Application configuration should go into files in config/initializers
     # -- all .rb files in that directory are automatically loaded.
-
-    # The default locale is :en and all translations from config/locales/*.rb,yml are auto loaded.
-    # config.i18n.load_path += Dir[Rails.root.join('my', 'locales', '*.{rb,yml}').to_s]
-    # config.i18n.default_locale = :de
-
-    # Do not swallow errors in after_commit/after_rollback callbacks.
-    config.active_record.raise_in_transactional_callbacks = true
-
-    # Load runner tasks.
-    config.autoload_paths += %W(#{config.root}/app/lib)
-    config.autoload_paths += %W(#{config.root}/lib)
-
-    # Time zone.
-    config.time_zone = 'Tokyo'
-
   end
 end
-
--- config/initializers/session_store.rb
+++ config/initializers/session_store.rb
@@ -1,3 +1,3 @@
 # Be sure to restart your server when you modify this file.
 
-Rails.application.config.session_store :cookie_store, key: '_cloud-sync-service_session'
+Rails.application.config.session_store :cookie_store, key: '_cloud_sync_service_session'
