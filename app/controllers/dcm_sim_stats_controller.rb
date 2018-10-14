12d53dd2f6cb07a25a1645f77d085b5e5c0a889f
Aki.SHIMO <fezrestia@gmail.com>
2018-10-14 21:39:24 +0900

    MOD: Change web scraping retry interval
    Change sleep interval from 3 to 1.

--- app/controllers/dcm_sim_stats_controller.rb
+++ app/controllers/dcm_sim_stats_controller.rb
@@ -218,7 +218,7 @@ class DcmSimStatsController < ApplicationController
     end
 
     MAX_PARSE_RETRY_COUNT = 3
-    RETRY_WAIT_SEC = 3
+    RETRY_WAIT_SEC = 1
 
     def get_with_retry
       raise "No block given." if !block_given?
